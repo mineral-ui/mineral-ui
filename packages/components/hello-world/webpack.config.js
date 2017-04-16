da3a1f5b725911c5ec3d6dfe73cfd28ccbc84e39
Brent Ertz <brent.ertz@gmail.com>
2017-04-16 08:16:57 -0600

    Externalize Hello and World packages from HelloWorld builds
    * Hello and World are peerDependencies of HelloWorld, which must be included separately rather than being bundled.
    * This does not apply to “demo” builds which just bundle everything together

--- packages/components/hello-world/webpack.config.js
+++ packages/components/hello-world/webpack.config.js
@@ -1,6 +1,30 @@
+const webpackMerge = require('webpack-merge');
 const makeWebpackConfig = require('../../../utils/makeWebpackConfig');
+const TARGET = process.env.TARGET;
 
-module.exports = makeWebpackConfig({
+const baseConfig = makeWebpackConfig({
   packageName: 'HelloWorld',
   packagePath: __dirname
 });
+
+let config = baseConfig;
+if (TARGET !== 'demo') {
+  config = webpackMerge(baseConfig, {
+    externals: {
+      '@mineral-ui/hello': {
+        root: 'Hello',
+        commonjs: '@mineral-ui/hello',
+        commonjs2: '@mineral-ui/hello',
+        amd: '@mineral-ui/hello'
+      },
+      '@mineral-ui/world': {
+        root: 'World',
+        commonjs: '@mineral-ui/world',
+        commonjs2: '@mineral-ui/world',
+        amd: '@mineral-ui/world'
+      }
+    }
+  });
+}
+
+module.exports = config;
