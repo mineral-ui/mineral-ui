556511e9762ab2184be58572ac602eb4b7cddda8
Brent Ertz <brent.ertz@gmail.com>
2017-04-27 12:22:06 -0600

    chore(license): Update license to Apache2
    * Updates license section of readme
    * Updates package.json license fields
    * Updates LICENSE.md files
    * Adds license header to source files
    * Updates “format” npm-script to inject license headers into js source files
    * Adds compact license header to dist files

---
+++ scripts/injectLicense.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2017 CA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Script to inject license header into source files.
+ *
+ * Usage: node ./inject-license
+ */
+const fs = require('fs');
+const glob = require('glob');
+const prependFile = require('prepend-file');
+let license = require('../utils/license').licenseHeader;
+
+license = `${license}\n`;
+
+const files = glob.sync('**/*.js', {
+  ignore: ['**/node_modules/**', '**/dist/**', '**/reports/**']
+});
+
+files.forEach(file => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    if (!data.includes(license)) {
+      prependFile(file, license, err => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  });
+});
--- utils/makeWebpackConfig.js
+++ utils/makeWebpackConfig.js
@@ -1,8 +1,24 @@
+/**
+ * Copyright 2017 CA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
+const compactLicenseHeader = require('./license').compactLicenseHeader;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const TARGET = process.env.TARGET;
 const ANALYZE = process.env.ANALYZE;
@@ -101,6 +117,11 @@ function getPlugins() {
   let plugins = [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    }),
+    new webpack.BannerPlugin({
+      banner: compactLicenseHeader,
+      raw: true,
+      entryOnly: true
     })
   ];
 
@@ -136,7 +157,6 @@ function getPlugins() {
           screw_ie8: true,
           warnings: false
         },
-        comments: false,
         sourceMap: true
       })
     ]);
