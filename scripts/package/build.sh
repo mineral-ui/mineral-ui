cf7503fba0e7b27609651eeecec2ff5efd0614ad
Brent Ertz <brent.ertz@gmail.com>
2017-05-12 12:58:29 -0600

    build(*): Don’t build es modules in site package

--- scripts/package/build.sh
+++ scripts/package/build.sh
@@ -3,7 +3,9 @@
 NODE_ENV="${NODE_ENV:-production}"
 
 # es modules
-NODE_ENV=$NODE_ENV babel src --out-dir dist/es --ignore *.spec.js,__demo__ --source-maps --minified
+if [ "$TARGET" != 'site' ]; then
+  NODE_ENV=$NODE_ENV babel src --out-dir dist/es --ignore *.spec.js,__demo__ --source-maps --minified
+fi
 
 # umd
 TARGET=$TARGET NODE_ENV=$NODE_ENV webpack
