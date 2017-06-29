ec01da68ad1110d5476e63f45e2395fc4f6ceec9
Mike Holm <michael.holm@ca.com>
2017-06-29 09:37:26 -0600

    feat(site): change home page redirect from hello to button

--- packages/site/src/components/App.js
+++ packages/site/src/components/App.js
@@ -80,7 +80,7 @@ export default function App({ className, demos }: Props) {
               return <ComponentDoc {...selectedDemo} />;
             }}
           />
-          <Redirect from="/" to="/components/hello" />
+          <Redirect from="/" to="/components/button" />
         </Switch>
         <Footer />
       </Main>
