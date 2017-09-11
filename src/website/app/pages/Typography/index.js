97f7ba630d41803c69d56ea2203de456e140d765
Riley Davis <riley.davis@ca.com>
2017-09-11 12:36:05 -0600

    feat(site): Typography page with some demos and tokens
    - add a Typography page
    - show examples of prescribed fonts
    - add color to Heading component
    - create a Paragraph component for future use
    - highlight some type-related tokens

---
+++ src/website/app/pages/Typography/index.js
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2017 CA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* @flow */
+import React from 'react';
+import colorable from 'colorable';
+import { createStyledComponent } from '../../../../utils';
+import Markdown from '../../Markdown';
+import {
+  Table as _Table,
+  TableCell,
+  TableHeaderCell,
+  TableRow
+} from '../../Table';
+import FontDemo from './FontDemo';
+import content from './typography.md';
+import examples from './examples';
+
+const a11yColor = color => {
+  const a11y = colorable({
+    main: color,
+    white: '#fff',
+    black: '#000',
+    compact: true
+  });
+
+  const whiteContrast = a11y[0].combinations[0].contrast;
+  const blackContrast = a11y[0].combinations[1].contrast;
+
+  return whiteContrast > blackContrast ? 'white' : 'black';
+};
+
+const styles = {
+  colorValue: ({ theme, color }) => {
+    return {
+      backgroundColor: theme[color],
+      borderRadius: theme.borderRadius_1,
+      color: a11yColor(theme[color]),
+      padding: `${theme.spacing_half} ${theme.spacing_single}`
+    };
+  },
+  overflowContainer: {
+    '@media(max-width: 60em)': {
+      overflowX: 'auto'
+    }
+  },
+  table: {
+    fontFamily: 'Open Sans'
+  },
+  valueCell: ({ theme }) => ({
+    fontFamily: theme.fontFamily_monospace,
+    verticalAlign: 'middle'
+  })
+};
+
+const ColorValue = createStyledComponent('span', styles.colorValue);
+const OverflowContainer = createStyledComponent(
+  'div',
+  styles.overflowContainer
+);
+const Table = createStyledComponent(_Table, styles.table);
+const ValueCell = createStyledComponent(TableCell, styles.valueCell);
+
+export default function Typography() {
+  return (
+    <div>
+      <Markdown scope={{ FontDemo }}>
+        {content}
+      </Markdown>
+      {examples.map((section, i) => {
+        return (
+          <section key={`section_${i}`}>
+            {section.heading}
+            {section.description}
+            <OverflowContainer>
+              <Table>
+                <thead>
+                  <tr>
+                    <TableHeaderCell width={20} scope="col">
+                      Example
+                    </TableHeaderCell>
+                    <TableHeaderCell width={8} scope="col">
+                      Value
+                    </TableHeaderCell>
+                    <TableHeaderCell scope="col">Color Value</TableHeaderCell>
+                    <TableHeaderCell scope="col">Font (px/rem)</TableHeaderCell>
+                  </tr>
+                </thead>
+                <tbody>
+                  {section.examples.map(({ content, value, color, font }) =>
+                    <TableRow key={value}>
+                      <TableCell>
+                        {content}
+                      </TableCell>
+                      <ValueCell>
+                        {value}
+                      </ValueCell>
+                      <ValueCell>
+                        <ColorValue color={color}>
+                          {`theme.${color}`}
+                        </ColorValue>
+                      </ValueCell>
+                      <ValueCell>
+                        {font}
+                      </ValueCell>
+                    </TableRow>
+                  )}
+                </tbody>
+              </Table>
+            </OverflowContainer>
+          </section>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/website/app/pages/Typography/typography.md
@@ -0,0 +1,34 @@
+# Typography
+
+Mineral UI provides a simple set of typographic elements to easily apply structure to your interface.
+Using consistent typographic styles will create clear paths for users to move through your application.
+
+## Font Usage
+
+Mineral UI uses [Open Sans](https://fonts.google.com/specimen/Open+Sans), a humanist sans-serif typeface with good legibility and warmth.
+Featuring a tall x-height and open forms, it is ideal for on-screen consumption.
+Open Sans supports ISO Latin 1, Latin CE, Greek and Cyrillic character sets.
+
+For Internationalization, support is included for RTL (right-to-left) languages. Asian Syllabic, Abjad, Indic and other script languages will be supported by sans-serif system font.
+
+<FontDemo />
+
+## Type Sizing
+
+Consistent application of typography helps the user understand information hierarchy.
+- Type sizing is based on a [typescale](http://spencermortensen.com/articles/typographic-scale/).
+- Type basis defines `16px` as `1rem`.
+- Type sizing is based on a `1.25` (4:5 Major-Third) scale. This ratio balances vertical density with an expression of clear hierarchy.
+- The root base (default text size) is `14px`.
+
+## Relative Line Spacing
+
+Linespacing is consistently set at `1.25` for all type sizes, except for prose text, which has a line height of `1.5` to increase readability for long-form text.
+
+## Hierarchy and Intent
+
+Consistency is key to creating an easily scannable interface for users.
+Each level in the Mineral UI type hierarchy is defined with a specific color and intent.
+Associating the typographic style to the intent in the interface provides a visual pattern that is easy for users to skim.
+
+The variations help keep type styles to a minimum, so consistency is easier to achieve. Too many type styles make a layout unbalanced and difficult to manage.
