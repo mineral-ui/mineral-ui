bd965b7b8979db5040df65c112c6f989282d403e
Brent Ertz <brent.ertz@ca.com>
2017-10-10 09:44:58 -0600

    docs(changelog): Fix duplicate entries in changelog
    * Manually update changelog to remove site/website commits
    * Ensure only new entries will be prepended to the changelog

--- utils/changelogConfig.js
+++ utils/changelogConfig.js
@@ -16,7 +16,15 @@
 
 /* @flow */
 module.exports = {
-  gitRawCommitsOpts: {
-    from: 'e293ace5ce534fe5a57306eb737db57943aa99e4' // Since mineral-ui package created
-  }
+  /**
+   * When enabled, the following configuration setting will generate all
+   * changelog entries starting from the creation of the mineral-ui package and
+   * prepend them to the changelog. This should only be run against a new/blank
+   * changelog.
+   *
+   * When disabled, only new entries will be prepended to the changelog.
+   */
+  // gitRawCommitsOpts: {
+  //   from: 'e293ace5ce534fe5a57306eb737db57943aa99e4' // mineral-ui package created
+  // }
 };
