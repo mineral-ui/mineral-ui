854061ba62ec03c1295498ff01983ad9c1c7ff4b
kylegach <kyle.gach@ca.com>
2017-10-11 16:33:17 -0600

    feat(icon,menu-item): Update variant icons

--- src/Icon/custom/IconSuccess.js
+++ src/Icon/custom/IconSuccess.js
@@ -38,7 +38,7 @@ export default function IconSuccess(props: Props) {
         <path
           d="M12 3c4.968 0 9 4.032 9 9s-4.032 9-9 9-9-4.032-9-9 4.032-9 9-9zm-4.247 8.445L6.5 12.698l3.838 3.838 7.198-7.198-1.253-1.254-5.945 5.945-2.585-2.585z"
           fill="currentColor"
-          fillRule="nonZero"
+          fillRule="nonzero"
         />
       </g>
     </Icon>
