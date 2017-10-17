40358843b332b8f2fa400a899e5e793f82d96cd8
kylegach <kyle.gach@ca.com>
2017-10-17 14:49:40 -0600

    refactor(utils): Refactor componentStyleReset to allow for no non-system font

--- src/utils/componentStyleReset.js
+++ src/utils/componentStyleReset.js
@@ -19,7 +19,9 @@ export default ({ theme }: Object) => {
   return {
     boxSizing: 'border-box',
     color: theme.color_text,
-    fontFamily: `${theme.fontFamily}, ${theme.fontFamily_system}`,
+    fontFamily: theme.fontFamily
+      ? `${theme.fontFamily}, ${theme.fontFamily_system}`
+      : `${theme.fontFamily_system}`,
     fontSize: `${theme.fontSize_base}px`,
     lineHeight: theme.lineHeight,
     outline: 0,
