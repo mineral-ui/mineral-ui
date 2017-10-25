eda31bcc4caf1f821b0898cce5a77a6a3a537bd1
alternatekev <alternatekev@me.com>
2017-10-24 21:23:11 -0600

    feat(website): shiny rock components

--- src/website/app/pages/Home/Rock.js
+++ src/website/app/pages/Home/Rock.js
@@ -8,6 +8,61 @@ export default class Rock extends Component {
     parallaxController: PropTypes.object,
   }
 
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      scale: 0.02,
+      left: 25,
+      top: -25,
+      opacity: 0,
+      transition: 'all 500ms ease-in',
+    }
+  }
+
+
+  componentDidMount = () => {
+
+    const min = 5000;
+    const max = 9000;
+    const delay = Math.floor( Math.random() * ( max - min ) ) + min;
+
+    setInterval( () => {
+      this.setState({
+        scale: 1,
+        left: -25,
+        top: 0,
+        opacity: 0.5,
+        transition: 'all 500ms ease-out',
+
+      });
+    }, delay );
+
+    setInterval( () => {
+      this.setState({
+        scale: 0.2,
+        left: -50,
+        top: 50,
+        opacity: 0,
+        transition: 'all 200ms ease-in',
+      });
+    }, delay + 300 );
+
+    setInterval( () => {
+      setTimeout( () => {
+        this.setState({
+          scale: 0.2,
+          left: -50,
+          top: 50,
+          opacity: 0,
+          transition: 'all 200ms ease-in',
+        });
+      }, delay )
+    }, delay + 20000 );
+
+  }
+
   render(){
     const imgSrc = `/public/images/rocks/SVG/${ this.props.type }.svg`;
 
@@ -27,6 +82,18 @@ export default class Rock extends Component {
             offsetXMin={ 0 }
             disabed={ !this.props.float }
             >
+              <div className="shine" ref="shine" style={{
+                borderRadius: '100%',
+                opacity: this.state.opacity,
+                position: 'absolute',
+                width: this.props.size,
+                height: this.props.size,
+                top: this.state.top,
+                left: this.state.left,
+                transform: `scale( ${ this.state.scale } )`,
+                transition: this.state.transition,
+                background: 'radial-gradient(ellipse at center, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 100%)',
+                }}></div>
               <img src={ imgSrc } style={ {
                 width: this.props.size,
                 height: 'auto'
@@ -49,7 +116,7 @@ export default class Rock extends Component {
     }
 
   }
-}
+};
 
 Rock.propTypes = {
   type: PropTypes.oneOf( [ 'Ruby', 'Tourmaline', 'Gold', 'Rockpile' ] ),
