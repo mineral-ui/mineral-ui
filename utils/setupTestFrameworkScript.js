a17a421e91ccfcb3442a059eabab58e2f5ed90cb
Brent Ertz <brent.ertz@ca.com>
2017-11-01 12:36:49 -0600

    build(test): Add jest:react npm script

--- utils/setupTestFrameworkScript.js
+++ utils/setupTestFrameworkScript.js
@@ -13,12 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import React from 'react';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
 import serializer from 'jest-glamor-react';
 import { simulations } from 'glamor';
+import semver from 'semver';
 
-// Configure Enzyme for our version of React
+// Configure Enzyme for appropriate React version
+let Adapter;
+if (semver.satisfies(React.version, '15.0.0 - 15.4.x')) {
+  Adapter = require('enzyme-adapter-react-15.4');
+} else if (semver.satisfies(React.version, '^15.5.0')) {
+  Adapter = require('enzyme-adapter-react-15');
+} else if (semver.satisfies(React.version, '^16.0.0')) {
+  Adapter = require('enzyme-adapter-react-16');
+}
 Enzyme.configure({ adapter: new Adapter() });
 
 // Include Glamor generated CSS instead of just classNames
