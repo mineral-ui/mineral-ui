b3329cf588e77bf8cb8faeb4b2e2c0060a576b22
kylegach <kyle.gach@ca.com>
2018-02-07 11:47:23 -0700

    chore(styles): Add getResponsiveStyles

--- src/styles/index.js
+++ src/styles/index.js
@@ -17,4 +17,5 @@
 /* @flow */
 export { default as createStyledComponent } from './createStyledComponent';
 export { default as getNormalizedValue } from './getNormalizedValue';
+export { default as getResponsiveStyles } from './getResponsiveStyles';
 export { default as pxToEm } from './pxToEm';
