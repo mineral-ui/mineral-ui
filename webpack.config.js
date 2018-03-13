d6c7d7161713a34586e566e748b21f83af4ccbf9
Brent Ertz <brent.ertz@ca.com>
2018-03-13 12:12:09 -0600

    chore(license): Remove license header from individual source files

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,10 @@
-/**
- * Copyright 2017 CA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* @flow */
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const compactLicenseHeader = require('./utils/license').compactLicenseHeader;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const ANALYZE = process.env.ANALYZE;
 const isProduction = NODE_ENV === 'production';
@@ -27,6 +12,11 @@ const isDevServer = process.argv.find(arg =>
   arg.includes('webpack-dev-server')
 );
 const GOOGLE_TRACKING_ID = isProduction ? 'UA-107538931-1' : null;
+const licenseHeader = `/**!
+ * Mineral UI
+ * Copyright 2017 CA
+ * License: Apache-2.0
+ */`;
 
 module.exports = {
   entry: {
@@ -74,7 +64,7 @@ module.exports = {
         GOOGLE_TRACKING_ID: JSON.stringify(GOOGLE_TRACKING_ID)
       }),
       new webpack.BannerPlugin({
-        banner: compactLicenseHeader,
+        banner: licenseHeader,
         raw: true,
         entryOnly: true
       }),
