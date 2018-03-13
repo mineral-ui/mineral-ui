884c458adecfe3f3247f2f3ad520fa0d1fcf3cf0
kylegach <kyle.gach@ca.com>
2018-03-13 11:35:57 -0600

    refactor(themes): Update theme variables
    General changes:
    - Change key naming scheme from [property]_[target]_[variant]_[state] to [target]_[property]_[variant]_[state]
    - Add "panel" and "well" targets
    - Add "inverted" concept
    - Add "icon" target
    - Re-order keys
    
    Specific changes:
    - Add `backgroundColor_disabled` (with different value)
    - Add `backgroundColor_theme_selected[state]`
    - Add `backgroundColor_themePrimary_[state]`
    - Add `backgroundColor_[state]`
    - Add `backgroundColor_[variant]_hover`
    - Add `borderColor_theme`
    - Add `boxShadow_focusInner`
    - Add `color_inverted`
    - Add `color_readOnly`
    - Add `color_required`
    - Add `icon_color` (with different value from previous Icon default)
    - Add `icon_color_theme`
    - Add `icon_color_[variant]`
    - Add `panel_backgroundColor`
    - Add `panel_backgroundColor_inverted`
    - Add `panel_borderColor`
    - Add `panel_borderColor_inverted`
    
    Co-authored-by: Victoria Vasys <victoria.vasys@gmail.com>
    
    BREAKING CHANGE: ([Detailed here](https://gist.github.com/kylegach/3f1300c071c8b47952eb12729e5a90e8)) Remove `color_caption`, `backgroundColor_link_focus`; rename: `backgroundColor_disabled` to `backgroundColor_input_disabled`, `backgroundColor_input` to `input_backgroundColor`, `backgroundColor_[variant]_muted` to `well_backgroundColor_[variant]`, `backgroundColor_[variant]_activeMuted` to `backgroundColor_[variant]_active`, `backgroundColor_[variant]_[state]` to `backgroundColor_[variant]Primary_[state]`, `borderColor_[state]` to `borderColor_theme_[state]`, `borderColor_[variant]_muted` to `well_borderColor_[variant]`, `color_h[#]` to `h[#]_color`, `color_placeholder` to `placeholder_color`, `color_text` to `color`, `color_text_disabled` to `color_disabled`, `color_text_primary_[state]` to `color_theme_[state]`, `color_text_onprimary` to `color_primary`, `color_text_[variant]_[state]` to `color_[variant]_[state]`, `fontSize_h[#]` to `h[#]_fontSize`, `fontWeight_h[#]` to `h[#]_fontWeight`, `shadow_[#]` to `boxShadow_[#]`

--- src/library/themes/createTheme.js
+++ src/library/themes/createTheme.js
@@ -1,217 +1,37 @@
 /* @flow */
-import color from '../colors';
+import colors from '../colors';
 import createColorRamp from './createColorRamp';
-import fontSize_base from './fontSizeBase';
-import pxToEm from '../styles/pxToEm';
+import tokens from './tokens';
 
 type Colors =
   | 'blue'
+  | 'bronze'
   | 'dusk'
   | 'gray'
   | 'green'
   | 'indigo'
-  | 'lime'
   | 'magenta'
-  | 'orange'
   | 'purple'
   | 'red'
   | 'sky'
   | 'slate'
-  | 'teal'
-  | 'yellow';
+  | 'teal';
 
-const spacing_quarter = pxToEm(2);
-const spacing_half = pxToEm(4);
-const spacing_1x = pxToEm(8);
-const spacing_2x = pxToEm(16);
-const spacing_3x = pxToEm(24);
-const spacing_4x = pxToEm(32);
-const spacing_8x = pxToEm(64);
-
-const grays = createColorRamp('gray', 'color_gray', color);
+export const defaultBaseColor = 'blue';
 
 export default function createTheme(
-  baseColor?: Colors = 'blue',
+  baseColor?: Colors = defaultBaseColor,
   overrides?: { [string]: any } = {}
 ): { [string]: any } {
-  /* Theme key naming convention:
-   *
-   *   [property]_[target]_[variant]_[state]
-   *
-   * E.g., A key for the border-color on focused "success" buttons and inputs
-   * would look like the following.
-   *
-   *   borderColor_success_focus
-   *
-   * Only [property] is required. Irrelevant parts are skipped.
-   */
+  const themeRamp = createColorRamp(baseColor, 'color_theme', colors);
 
-  const primaries = createColorRamp(baseColor, 'color_theme', color);
-
-  // prettier-ignore
   return {
-    backgroundColor_disabled: color.gray_20,
-    backgroundColor_danger: color.red_60,
-    backgroundColor_danger_muted: color.red_20,
-    backgroundColor_danger_active: color.red_70,
-    backgroundColor_danger_activeMuted: color.red_30,
-    backgroundColor_danger_focus: color.red_60,
-    backgroundColor_danger_hover: color.red_50,
-    backgroundColor_success: color.green_80,
-    backgroundColor_success_muted: color.green_20,
-    backgroundColor_success_active: color.green_90,
-    backgroundColor_success_activeMuted: color.green_30,
-    backgroundColor_success_focus: color.green_80,
-    backgroundColor_success_hover: color.green_70,
-    backgroundColor_warning: color.orange_60,
-    backgroundColor_warning_muted: color.orange_20,
-    backgroundColor_warning_active: color.orange_70,
-    backgroundColor_warning_activeMuted: color.orange_30,
-    backgroundColor_warning_focus: color.orange_60,
-    backgroundColor_warning_hover: color.orange_50,
-
-    backgroundColor_input: color.white,
-
-    backgroundColor_link_focus: color.gray_30,
-
-    borderColor: color.gray_40,
-    borderColor_active: primaries.color_theme_60,
-    borderColor_focus: primaries.color_theme_60,
-    borderColor_hover: primaries.color_theme_60,
-    borderColor_danger: color.red_60,
-    borderColor_danger_active: color.red_70,
-    borderColor_danger_focus: color.red_70,
-    borderColor_danger_hover: color.red_70,
-    borderColor_danger_muted: color.red_40,
-    borderColor_success: color.green_70,
-    borderColor_success_active: color.green_80,
-    borderColor_success_focus: color.green_80,
-    borderColor_success_hover: color.green_80,
-    borderColor_success_muted: color.green_40,
-    borderColor_warning: color.orange_60,
-    borderColor_warning_active: color.orange_70,
-    borderColor_warning_focus: color.orange_70,
-    borderColor_warning_hover: color.orange_70,
-    borderColor_warning_muted: color.orange_40,
-
-    borderRadius_1: pxToEm(3),
-
-    breakpoint_narrow: 512,
-    breakpoint_medium: 768,
-    breakpoint_wide: 1024,
-
-    color_black: color.black,
-
-    color_caption: color.gray_80,
-
-    ...grays,
-
-    color_h1: color.gray_100,
-    color_h2: color.gray_80,
-    color_h3: color.gray_80,
-    color_h4: color.gray_80,
-    color_h5: color.gray_100,
-    color_h6: color.gray_80,
-    color_mouse: color.gray_80,
-    color_placeholder: color.gray_60,
-
-    ...primaries,
-
-    color_text: color.gray_100,
-    color_text_disabled: color.gray_50,
-    color_text_danger: color.red_60,
-    color_text_danger_active: color.red_70,
-    color_text_danger_focus: color.red_60,
-    color_text_danger_hover: color.red_50,
-    color_text_ondanger: color.white,
-    color_text_primary: primaries.color_theme_60,
-    color_text_primary_active: primaries.color_theme_70,
-    color_text_primary_focus: primaries.color_theme_60,
-    color_text_primary_hover: primaries.color_theme_50,
-    color_text_onprimary: color.white,
-    color_text_success: color.green_90,
-    color_text_success_active: color.green_100,
-    color_text_success_focus: color.green_90,
-    color_text_success_hover: color.green_80,
-    color_text_onsuccess: color.white,
-    color_text_warning: color.orange_70,
-    color_text_warning_active: color.orange_80,
-    color_text_warning_focus: color.orange_70,
-    color_text_warning_hover: color.orange_60,
-    color_text_onwarning: color.white,
-
-    color_white: color.white,
-
-    direction: 'ltr',
-
-    fontFamily: 'Open Sans',
-    fontFamily_system: '-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-    fontFamily_monospace: '"SF Mono", "Droid Sans Mono", "Source Code Pro", Monaco, Consolas, "Courier New", Courier, monospace',
-
-    fontSize_base,
-    fontSize_h1: pxToEm(34),
-    fontSize_h2: pxToEm(28),
-    fontSize_h3: pxToEm(22),
-    fontSize_h4: pxToEm(18),
-    fontSize_h5: pxToEm(14),
-    fontSize_h6: pxToEm(14),
-    fontSize_mouse: pxToEm(11),
-    fontSize_prose: pxToEm(16),
-    fontSize_ui: pxToEm(14),
-
-    fontWeight_regular: 400,
-    fontWeight_semiBold: 600,
-    fontWeight_bold: 700,
-    fontWeight_extraBold: 800,
-    fontWeight_h1: 800,
-    fontWeight_h2: 700,
-    fontWeight_h3: 700,
-    fontWeight_h4: 700,
-    fontWeight_h5: 700,
-    fontWeight_h6: 400,
-
-    lineHeight: 1.25,
-    lineHeight_heading: 1.25,
-    lineHeight_heading_small: 1.5,
-    lineHeight_prose: 1.5,
-
-    shadow_1: '0 1px 2px 0 rgba(0,0,0,0.2), 0 2px 4px 0 rgba(0,0,0,0.2)',
-    shadow_2: '0 2px 4px 0 rgba(0,0,0,0.2), 0 4px 8px 0 rgba(0,0,0,0.2)',
-    shadow_3: '0 4px 8px 0 rgba(0,0,0,0.2), 0 8px 16px 0 rgba(0,0,0,0.2)',
-    shadow_4: '0 8px 16px 0 rgba(0,0,0,0.2), 0 20px 16px -8px rgba(0,0,0,0.2)',
-    shadow_5: '0 16px 24px 0 rgba(0,0,0,0.2), 0 32px 24px -16px rgba(0,0,0,0.2)',
-
-    size_small: pxToEm(24),
-    size_medium: pxToEm(32),
-    size_large: pxToEm(40),
-    size_jumbo: pxToEm(52),
-
-    space_inline_xxs: spacing_quarter,
-    space_inline_xs: spacing_half,
-    space_inline_sm: spacing_1x,
-    space_inline_md: spacing_2x,
-    space_inline_lg: spacing_3x,
-    space_inline_xl: spacing_4x,
-    space_inline_xxl: spacing_8x,
-
-    space_inset_sm: spacing_1x,
-    space_inset_md: spacing_2x,
-    space_inset_lg: spacing_3x,
-
-    space_stack_xxs: spacing_quarter,
-    space_stack_xs: spacing_half,
-    space_stack_sm: spacing_1x,
-    space_stack_md: spacing_2x,
-    space_stack_lg: spacing_3x,
-    space_stack_xl: spacing_4x,
-    space_stack_xxl: spacing_8x,
-
-    zIndex_100: 100,
-    zIndex_200: 200,
-    zIndex_400: 400,
-    zIndex_800: 800,
-    zIndex_1600: 1600,
-
+    ...tokens.reduce((acc, token) => {
+      const values = Object.values(token)[0];
+      const returnedValues =
+        typeof values === 'function' ? values(themeRamp) : values;
+      return Object.assign(acc, returnedValues);
+    }, {}),
     ...overrides
   };
 }
