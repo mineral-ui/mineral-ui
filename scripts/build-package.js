645c8278e7b935054eafd603f766edb8c26b5ca6
Brent Ertz <brent.ertz@ca.com>
2018-03-20 06:37:11 -0600

    chore(scripts): Improve build/release scripts

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,34 +1,125 @@
 #! /usr/bin/env node
 /* eslint-disable no-console */
 
-const execSync = require('child_process').execSync;
+const os = require('os');
+const fs = require('fs-extra');
+const path = require('path');
+const { execSync } = require('child_process');
+const semver = require('semver');
 
-const { NODE_ENV = 'production' } = process.env;
+// Configure the arguments parser and documentation
+const argv = require('yargs')
+  .option('label', {
+    describe: 'Build a pre-release package with a custom version label'
+  })
+  .option('skip-git', {
+    describe: 'Skip git commands. No commits, tags, etc.'
+  })
+  .usage('Usage: $0 [options]')
+  .example('$0', 'Build a standard package.')
+  .example(
+    '$0 --label alpha',
+    'Build a canary/prerelease package with label "alpha".  e.g. 0.1.0-alpha.0'
+  )
+  .example(
+    '$0 --skip-git',
+    'Build a package without committing or tagging anything.'
+  )
+  .help('h')
+  .alias('h', 'help').argv;
 
-const exec = (command, env) =>
+const exec = command =>
   execSync(command, {
-    stdio: 'inherit',
-    env: { ...process.env, ...env }
+    stdio: 'inherit'
   });
 
-// components es modules
-console.log('\n\nBuilding ES modules...');
-exec(
-  'babel ./src/library --out-dir ./dist/es --ignore *.spec.js,*.template.js',
-  {
-    NODE_ENV
-  }
-);
+console.log('\n\nBuilding package...');
+const { label, skipGit } = argv;
 
-// components cjs modules
-console.log('\n\nBuilding CommonJS modules...');
-exec('babel ./src/library --out-dir ./dist --ignore *.spec.js,*.template.js', {
-  BABEL_ENV: 'cjs',
-  NODE_ENV
+// Read root package.json and get current package version
+const packageJsonFile = path.resolve(__dirname, '../package.json');
+const packageJsonData = fs.readJsonSync(packageJsonFile);
+const { version: currentVersion } = packageJsonData;
+
+// Determine next package version
+let nextVersion;
+if (label) {
+  // Adjust pre-release version
+  const bump = semver.prerelease(currentVersion) ? 'prerelease' : 'preminor';
+  nextVersion = semver.inc(currentVersion, bump, label); // e.g. 0.1.0-alpha.0
+} else {
+  // Simply increment minor version until 1.0 milestone
+  // Following the 1.0 milestone, this can be updated with something that
+  // determines the version bump based on commit messages
+  nextVersion = semver.inc(currentVersion, 'minor');
+}
+
+// Create a copy of the original package.json for restoration later
+exec('cp package.json _package.json');
+
+// Bump version in package.json, but do not commit or tag it
+// This is necessary in order to generate a changelog with the appropriate version
+exec(`npm --no-git-tag-version version ${nextVersion}`);
+
+// Generate changelog
+const changelogConfigFile = path.resolve(
+  __dirname,
+  '../utils/changelogConfig.js'
+);
+exec(
+  `conventional-changelog --preset angular --infile CHANGELOG.md --same-file --config ${changelogConfigFile}`
+);
+// Check if a changelog has been generated
+// Note: tmpfile used to get output from git status command
+const gitStatusFile = path.resolve(os.tmpdir(), '.git-status');
+exec(`echo "$(git status --porcelain)" > ${gitStatusFile}`);
+const hasChangelog = fs
+  .readFileSync(gitStatusFile, { encoding: 'utf8' })
+  .includes('CHANGELOG.md');
+fs.removeSync(gitStatusFile);
+// Only commit changelog if it has been updated/generated
+if (hasChangelog && !skipGit) {
+  exec('git add CHANGELOG.md');
+  exec(`git commit -m "docs(changelog): ${nextVersion}"`);
+}
+
+// Restore original package.json
+exec('mv -f _package.json package.json');
+
+// Update version in package.json, commit and tag
+exec(`npm version ${nextVersion} --no-git-tag-version`);
+if (!skipGit) {
+  exec(`git add package.json`);
+  exec(`git commit -m "chore(release): ${nextVersion}"`);
+  exec(`git tag -f v${nextVersion}`);
+}
+
+// Copy additional files to dist to be included in the npm package
+exec('cp {README.md,LICENSE.md,CHANGELOG.md} dist');
+
+// Create simplified package.json in dist
+const updatedPackageJasonData = fs.readJsonSync(packageJsonFile);
+const minimalPackageJsonData = {
+  name: updatedPackageJasonData.name,
+  author: updatedPackageJasonData.author,
+  description: updatedPackageJasonData.description,
+  version: updatedPackageJasonData.version,
+  license: updatedPackageJasonData.license,
+  repository: updatedPackageJasonData.repository,
+  bugs: updatedPackageJasonData.bugs,
+  homepage: updatedPackageJasonData.homepage,
+  main: './index.js',
+  module: './es/index.js',
+  files: ['**', '!website', '!*.tgz'],
+  dependencies: updatedPackageJasonData.dependencies,
+  peerDependencies: updatedPackageJasonData.peerDependencies
+};
+const minimalPackageJsonFile = path.resolve(__dirname, '../dist/package.json');
+fs.writeJsonSync(minimalPackageJsonFile, minimalPackageJsonData, {
+  spaces: 2
 });
 
-// flow definitions - not yet ready for public consumption
-// console.log('\n\nCopying Flow Definitions...');
-// exec(
-//   'flow-copy-source -v -i "**/__tests__/**" -i "**/dist/**" -i "**/lib/**" -i "**/templates/**" ./src/library ./dist'
-// );
+// Create an installable package for local testing
+exec('cd dist && npm pack');
+
+console.log('Package built successfully.');
--- scripts/release.js
+++ scripts/release.js
@@ -2,33 +2,29 @@
 /* eslint-disable no-console */
 
 const os = require('os');
-const fse = require('fs-extra');
+const fs = require('fs-extra');
 const path = require('path');
 const { execSync } = require('child_process');
-const semver = require('semver');
 const inquirer = require('inquirer');
 
 // Configure the arguments parser and documentation
 const argv = require('yargs')
-  .option('canary', {
-    describe: 'Publish a pre-release canary version'
-  })
   .option('npm-tag', {
     describe: 'Publish a release using a custom npm dist-tag',
     default: 'latest',
     type: 'string'
   })
+  .option('skip-git', {
+    describe: "Skip git commands. Don't push to GitHub"
+  })
+  .option('skip-npm', {
+    describe: "Skip npm commands. Don't publish to npm"
+  })
   .usage('Usage: $0 [options]')
-  .example('$0', 'perform a standard release')
-  .example(
-    '$0 --canary',
-    'perform a canary/prerelease with label and npm dist-tag "alpha", e.g. 0.1.0-alpha.0'
-  )
-  .example(
-    '$0 --canary=beta --npmTag=beta',
-    'perform a canary/prerelease with label and npm dist-tag "beta", e.g. 0.1.0-beta.0'
-  )
-  .example('$0 --npmTag=next', 'perform a release using npm dist-tag "next"')
+  .example('$0', 'Perform a standard release')
+  .example('$0 --npm-tag=next', 'Perform a release using npm dist-tag "next"')
+  .example('$0 --skip-git', 'Skip push to GitHub.')
+  .example('$0 --skip-npm', 'Skip publish to npm.')
   .help('h')
   .alias('h', 'help').argv;
 
@@ -37,85 +33,32 @@ const exec = command =>
     stdio: 'inherit'
   });
 
-// Read root package.json and get current package version
-const packageJsonFile = path.resolve(__dirname, '../package.json');
-const packageJsonData = fse.readJsonSync(packageJsonFile);
-const { version: currentVersion } = packageJsonData;
+const { npmTag, skipGit, skipNpm } = argv;
 
-// Identify next package version - simply increment minor version until 1.0 milestone
-// Following the 1.0 milestone, this can be updated with something that
-// determines the version bump based on commit messages
-let nextVersion = semver.inc(currentVersion, 'minor');
-
-// Avoid publishing canary under latest npm-tag, default to alpha
-const npmTag = argv.canary && argv.npmTag === 'latest' ? 'alpha' : argv.npmTag;
-
-// Adjust canary version appropriately
-if (argv.canary) {
-  const label = typeof argv.canary === 'string' ? argv.canary : 'alpha';
-  const bump = semver.prerelease(currentVersion) ? 'prerelease' : 'preminor';
-  nextVersion = semver.inc(currentVersion, bump, label); // e.g. - 0.1.0-alpha.0
+// Read dist/package.json and get package version
+const packageJsonFile = path.resolve(__dirname, '../dist/package.json');
+if (!fs.existsSync(packageJsonFile)) {
+  console.log(
+    `Unable to find ${packageJsonFile}. You must build a package first. Release aborted.`
+  );
+  process.exit(0);
 }
+const version = fs.readJsonSync(packageJsonFile).version;
 
-// Create a copy of the original package.json for restoration later
-exec('cp package.json _package.json');
-
-// Bump version in package.json, but do not commit or tag it
-// This is necessary in order to generate a changelog with the appropriate version
-exec(`npm --no-git-tag-version version ${nextVersion}`);
-
-// Generate changelog
-const changelogConfigFile = path.resolve(
-  __dirname,
-  '../utils/changelogConfig.js'
-);
-exec(
-  `conventional-changelog --preset angular --infile CHANGELOG.md --same-file --config ${changelogConfigFile}`
-);
-// Check if a changelog has been generated
-// Note: tmpfile used to get output from git status command
-const gitStatusFile = path.resolve(os.tmpdir(), '.git-status');
-exec(`echo "$(git status --porcelain)" > ${gitStatusFile}`);
-const hasChangelog = fse
-  .readFileSync(gitStatusFile, { encoding: 'utf8' })
-  .includes('CHANGELOG.md');
-fse.removeSync(gitStatusFile);
-// Only commit changelog if it has been updated/generated
-if (hasChangelog) {
-  exec('git add CHANGELOG.md');
-  exec(`git commit -m "docs(changelog): ${nextVersion}"`);
+// Check if version has previously been published
+// Note: tmpfile used to get output from npm command
+const npmVersionsFile = path.resolve(os.tmpdir(), '.npm-versions');
+exec(`echo "$(npm show mineral-ui versions)" > ${npmVersionsFile}`);
+const versionPreviouslyPublished = fs
+  .readFileSync(npmVersionsFile, { encoding: 'utf8' })
+  .includes(version);
+fs.removeSync(npmVersionsFile);
+if (versionPreviouslyPublished) {
+  console.log(
+    `Version ${version} has previously been published. Release aborted.`
+  );
+  process.exit(0);
 }
-
-// Restore original package.json
-exec('mv -f _package.json package.json');
-
-// Update version in package.json, commit and tag
-exec(`npm version ${nextVersion} -f -m "chore(release): ${nextVersion}"`);
-
-// Copy additional files to dist to be included in the npm package
-exec('cp {README.md,LICENSE.md,CHANGELOG.md} dist');
-
-// Create simplified package.json in dist
-const updatedPackageJasonData = fse.readJsonSync(packageJsonFile);
-const minimalPackageJsonData = {
-  name: updatedPackageJasonData.name,
-  author: updatedPackageJasonData.author,
-  description: updatedPackageJasonData.description,
-  version: updatedPackageJasonData.version,
-  license: updatedPackageJasonData.license,
-  repository: updatedPackageJasonData.repository,
-  bugs: updatedPackageJasonData.bugs,
-  homepage: updatedPackageJasonData.homepage,
-  main: './index.js',
-  module: './es/index.js',
-  files: ['**', '!website'],
-  dependencies: updatedPackageJasonData.dependencies,
-  peerDependencies: updatedPackageJasonData.peerDependencies
-};
-const minimalPackageJsonFile = path.resolve(__dirname, '../dist/package.json');
-fse.writeJsonSync(minimalPackageJsonFile, minimalPackageJsonData, {
-  spaces: 2
-});
 
 // Publishing - prompt user for confirmation
 inquirer
@@ -123,18 +66,26 @@ inquirer
     {
       type: 'confirm',
       name: 'confirm',
-      message: `Ready to release ${nextVersion} using npm tag ${npmTag}?`
+      message: `Ready to release ${version} using npm tag ${npmTag}?`
     }
   ])
   .then(({ confirm }) => {
     if (confirm) {
-      // Push commits and tags to GitHub
-      console.log('Pushing to GitHub...');
-      exec('git push -f --follow-tags');
-
       // Publish to npm
-      console.log('Publishing to npm...');
-      exec(`cd dist && npm publish --tag=${npmTag}`);
+      if (skipNpm) {
+        console.log('Skipped publish to npm...');
+      } else {
+        console.log('Publishing to npm...');
+        exec(`cd dist && npm publish --tag=${npmTag}`);
+      }
+
+      // Push commits and tags to GitHub
+      if (skipGit) {
+        console.log('Skipped push to GitHub...');
+      } else {
+        console.log('Pushing to GitHub...');
+        exec('git push -f --follow-tags');
+      }
     } else {
       console.log('Release aborted.');
     }
