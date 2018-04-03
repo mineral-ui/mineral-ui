84847d5e0262107e054bc43d3670d78f573b7d2f
Victoria Vasys <victoria.vasys@gmail.com>
2018-04-03 10:56:21 -0600

    test(Text): Add spec & snapshots
    Co-authored-by: Kyle Gach <kyle.gach@ca.com>

---
+++ src/library/Text/__tests__/Text.spec.js
@@ -0,0 +1,20 @@
+/* @flow */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Text from '../Text';
+import examples from '../../../website/app/demos/Text/examples';
+import testDemoExamples from '../../../../utils/testDemoExamples';
+
+function shallowText(props) {
+  return shallow(<Text {...props}>A</Text>);
+}
+
+describe('Text', () => {
+  it('renders', () => {
+    const text = shallowText({});
+
+    expect(text.exists()).toEqual(true);
+  });
+
+  testDemoExamples(examples);
+});
