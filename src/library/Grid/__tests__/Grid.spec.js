930c3f1587b34535b0f55cd09f03e4718f43e825
kylegach <kyle.gach@ca.com>
2018-04-17 11:44:40 -0600

    test(flex,grid): Add specs and snapshots
    - Flex's gutter example was updated, which changed the snapshots

---
+++ src/library/Grid/__tests__/Grid.spec.js
@@ -0,0 +1,24 @@
+/* @flow */
+import React from 'react';
+import { shallow } from 'enzyme';
+import testDemoExamples from '../../../../utils/testDemoExamples';
+import Grid, { GridItem } from '../../Grid';
+import examples from '../../../website/app/demos/Grid/examples/Grid';
+
+function shallowGrid(gridProps, gridItemProps) {
+  return shallow(
+    <Grid {...gridProps}>
+      <GridItem {...gridItemProps} />
+    </Grid>
+  );
+}
+
+describe('Grid', () => {
+  it('renders', () => {
+    const grid = shallowGrid({}, {});
+
+    expect(grid.exists()).toEqual(true);
+  });
+
+  testDemoExamples(examples);
+});
---
+++ src/library/Grid/__tests__/GridItem.spec.js
@@ -0,0 +1,20 @@
+/* @flow */
+import React from 'react';
+import { shallow } from 'enzyme';
+import testDemoExamples from '../../../../utils/testDemoExamples';
+import { GridItem } from '../../Grid';
+import examples from '../../../website/app/demos/Grid/examples/GridItem';
+
+function shallowGridItem(props) {
+  return shallow(<GridItem {...props} />);
+}
+
+describe('GridItem', () => {
+  it('renders', () => {
+    const gridItem = shallowGridItem({});
+
+    expect(gridItem.exists()).toEqual(true);
+  });
+
+  testDemoExamples(examples);
+});
