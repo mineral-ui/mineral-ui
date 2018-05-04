19d6988cc299955396f143630a97e9826dea336f
Mike Holm <mholm@rallydev.com>
2018-05-04 12:36:24 -0600

    fix(styles): add tests and fix behavior of pxToEm
    BREAKING CHANGE: Only transform numbers or strings in px units.
    All other input errors.

--- src/library/styles/pxToEm.js
+++ src/library/styles/pxToEm.js
@@ -4,8 +4,22 @@
  */
 import fontSize_base from '../themes/fontSizeBase';
 
+const REGEX_NUM_OR_END_IN_PX = /^[\d.]+(px)?$/;
+
+const convert = (input: number): string =>
+  `${input / parseInt(fontSize_base)}em`;
+
+const errorMsg = (actual) => {
+  const value = typeof actual === 'string' ? actual : typeof actual;
+  return `[mineral-ui/styles/pxToEm]: Expected a number or string in px units. Instead got: '${value}'.`;
+};
+
 export default function pxToEm(value: number | string) {
-  const input =
-    typeof value === 'string' ? parseInt(value.replace('px', '')) : value;
-  return `${input / parseInt(fontSize_base)}em`;
+  if (typeof value === 'number') {
+    return convert(value);
+  }
+  if (typeof value === 'string' && REGEX_NUM_OR_END_IN_PX.test(value)) {
+    return convert(parseFloat(value));
+  }
+  throw new Error(errorMsg(value));
 }
