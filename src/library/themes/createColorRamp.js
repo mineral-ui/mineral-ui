37555c9b35c2ff5f821da8e66023bb566201ea2a
kylegach <kyle.gach@ca.com>
2018-05-08 15:56:03 -0600

    feat(themes): Generate values from color options
    - Add `color_[variant]Primary` theme variables
    - Update mineral-ui-tokens dependency
    
    Co-authored-by: Mike Holm <coldpour@gmail.com>
    
    BREAKING CHANGE: Change `createTheme` signature. Replace `baseColor` and `overrides` parameters with an options object, containing `colors` (baseColor is now colors.theme) and `overrides`. See the [new documentation](http://mineral-ui.com/theming#common-scenarios-api-{{47}}) for details. Rename `color_primary` theme variable to `color_themePrimary` for consistency with other variable names

--- src/library/themes/createColorRamp.js
+++ src/library/themes/createColorRamp.js
@@ -3,44 +3,31 @@
  * Generates an object of colors with renamed keys from a color palette.
  * This is primarily used to translate plain color objects into theme variables.
  *
- * Note that `inKey` is optional. If not provided, all keys in `colors` will be
- * renamed.
- *
  * e.g.
- *    createColorRamp(color, 'color_theme', 'blue');
+ *    createColorRamp(palette.blue, 'color_theme_');
  *
  *    returns
  *      {
- *        color_theme_10: '#e6eefc',
- *        color_theme_20: '#c2dbfc',
- *        color_theme_30: '#9dc2fa',
- *        color_theme_40: '#72a5f2',
- *        color_theme_50: '#4a89e8',
- *        color_theme_60: '#2e6fd9',
- *        color_theme_70: '#1f5dc2',
- *        color_theme_80: '#164ea8',
- *        color_theme_90: '#114091',
- *        color_theme_100: '#0f397d
+ *        color_theme_10: '#f0f5fc',
+ *        color_theme_20: '#cfe0fc',
+ *        color_theme_30: '#accbfc',
+ *        color_theme_40: '#84b1fa',
+ *        color_theme_50: '#5691f0',
+ *        color_theme_60: '#3272d9',
+ *        color_theme_70: '#1d5bbf',
+ *        color_theme_80: '#114599',
+ *        color_theme_90: '#103570',
+ *        color_theme_100: '#15233b'
  *      }
  */
 export default function createColorRamp(
-  colors: { [string]: any }, // The palette of colors
-  outKey: string, // The key of the color in the returned object, excluding the index
-  inKey?: string // The key of the color in the color palette, excluding the index
+  colors: { [number]: string }, // The palette of colors
+  outKey: string // The key of the color in the returned object, excluding the index
 ): {
-  [string]: any
+  [string]: string
 } {
-  let keys = Object.keys(colors);
-  let newKey = (key) => `${outKey}${key}`;
-
-  if (inKey) {
-    const REGEX_IN_KEY = new RegExp(`^${inKey}`);
-    keys = keys.filter((key) => REGEX_IN_KEY.test(key));
-    newKey = (key) => key.replace(REGEX_IN_KEY, outKey);
-  }
-
-  return keys.reduce((acc, key) => {
-    acc[newKey(key)] = colors[key];
+  return Object.keys(colors).reduce((acc, key) => {
+    acc[`${outKey}${key}`] = colors[parseInt(key)];
     return acc;
   }, {});
 }
--- src/library/themes/index.js
+++ src/library/themes/index.js
@@ -5,6 +5,7 @@ export { default as createThemedComponent } from './createThemedComponent';
 export { default as fontSizeBase } from './fontSizeBase';
 export { default as mapComponentThemes } from './mapComponentThemes';
 export { default as mineralTheme } from './mineralTheme';
+export { default as palette } from './generated/palette';
 export { default as themeFromTokens } from './themeFromTokens';
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as withTheme } from './withTheme';
