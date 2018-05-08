a2fd9dc2c5068dce88f20cf177662c04dcb2f533
kylegach <kyle.gach@ca.com>
2018-05-08 15:56:03 -0600

    feat(themes): Generate values from color options
    - Add `color_[variant]Primary` theme variables
    - Update mineral-ui-tokens dependency
    
    Co-authored-by: Mike Holm <coldpour@gmail.com>
    
    BREAKING CHANGE: Change `createTheme` signature. Replace `baseColor` and `overrides` parameters with an options object, containing `colors` (baseColor is now colors.theme) and `overrides`. See the [new documentation](http://mineral-ui.com/theming#common-scenarios-api-{{47}}) for details. Rename `color_primary` theme variable to `color_themePrimary` for consistency with other variable names

--- src/library/themes/createTheme.js
+++ src/library/themes/createTheme.js
@@ -1,56 +1,139 @@
 /* @flow */
-import tokens, { black, brand, gray, palette, white } from 'mineral-ui-tokens';
+import tokens from 'mineral-ui-tokens';
 import createColorRamp from './createColorRamp';
 import fontSize_base from './fontSizeBase';
-import themeFromTokens from './themeFromTokens';
+import colorAliases from './generated/colorAliases';
+import rampTokens from './generated/groupedByRampJsTokens';
+import palette, { type Color } from './generated/palette';
+import themeFromTokens, {
+  themeFromColorTokens,
+  type Ramp
+} from './themeFromTokens';
 
-type Colors =
-  | 'blue'
-  | 'bronze'
-  | 'dusk'
-  | 'gray'
-  | 'green'
-  | 'indigo'
-  | 'magenta'
-  | 'purple'
-  | 'red'
-  | 'sky'
-  | 'slate'
-  | 'teal';
-type Ramp = { [string]: string };
-
-const defaultBaseColor = 'blue';
-
-const grayRamp = createColorRamp(gray, 'color_gray_');
-
-const getThemeRamp = (themeRamp?: Ramp) => {
-  return themeRamp || createColorRamp(brand, 'color_theme_');
+type RampWithInflection = { [number]: string, inflection?: number };
+type Colors = {
+  black?: string,
+  danger?: Color | RampWithInflection,
+  gray?: RampWithInflection,
+  success?: Color | RampWithInflection,
+  theme?: Color | RampWithInflection,
+  warning?: Color | RampWithInflection,
+  white?: string
 };
+type Options = ?{
+  colors?: Colors,
+  overrides?: Object
+};
+type PrimaryColor = 'theme' | 'danger' | 'success' | 'warning';
 
-export const nonTokenVariables = {
-  boxShadow_focusInner: white,
+const primaryColors: Array<PrimaryColor> = [
+  'theme',
+  'danger',
+  'success',
+  'warning'
+];
+
+export const nonTokenVariables = (colors: ?Colors) => ({
+  boxShadow_focusInner: (colors && colors.white) || palette.white,
   direction: 'ltr',
   fontSize_base
+});
+
+const colorOverrides = (colors) =>
+  Object.keys(rampTokens).reduce((acc, color) => {
+    return colors[color]
+      ? {
+          ...acc,
+          ...themeFromColorTokens({
+            color: correctColorType(colors[color], color),
+            colorName: color,
+            tokens: rampTokens[color]
+          })
+        }
+      : acc;
+  }, {});
+
+const correctColorType = (
+  colorValue: string | RampWithInflection,
+  color: string
+): string | Ramp => {
+  const isStringColor = ['black', 'white'].indexOf(color) !== -1;
+
+  return typeof colorValue === 'string' && isStringColor
+    ? colorValue
+    : getRamp(colorValue, color);
 };
 
-export default function createTheme(
-  baseColor?: Colors = defaultBaseColor,
-  overrides?: { [string]: any } = {}
-): { [string]: any } {
-  let themeRamp;
-  if (baseColor !== defaultBaseColor) {
-    themeRamp = createColorRamp(palette, 'color_theme', baseColor);
+const getRamp = (color: string | RampWithInflection, keyName: string): Ramp => {
+  if (typeof color === 'string') {
+    if (palette[color]) {
+      return palette[color];
+    } else {
+      throw new Error(`[mineral-ui/themes/createTheme]: Expected colors.${keyName} to be a color ramp from the mineral-ui palette or a custom color ramp, but got '${color}'.
+See https://mineral-ui.com/color#guidelines-ramps for the palette ramps.
+See https://github.com/mineral-ui/mineral-ui/blob/master/packages/mineral-ui-tokens/src/blue.js for an example color ramp.`);
+    }
   }
 
+  const { inflection: ignoreInflection, ...ramp } = color;
+  return ramp;
+};
+
+const getWhiteOrBlackTextColor = (color, colors, override) => {
+  const black = colors.black || palette.black;
+  const white = colors.white || palette.white;
+
+  const inflectionPoints: { [PrimaryColor]: number } = primaryColors.reduce(
+    (acc, color) => {
+      acc[color] = colorAliases[
+        `backgroundColor_${color === 'theme' ? 'brand' : color}Primary`
+      ].split('_')[1];
+      return acc;
+    },
+    {}
+  );
+
+  return override && typeof override.inflection === 'number'
+    ? override.inflection > inflectionPoints[color] ? black : white
+    : white;
+};
+
+const primaryColorsByVariation = (colors?: Colors = {}) => {
+  return primaryColors.reduce((acc, color) => {
+    const override = colors[color];
+    acc[`color_${color}Primary`] = getWhiteOrBlackTextColor(
+      color,
+      colors,
+      override
+    );
+    return acc;
+  }, {});
+};
+
+export default function createTheme(options: Options): Object {
+  const colors = options && options.colors;
+
+  const grayRamp =
+    colors && colors.gray
+      ? createColorRamp(getRamp(colors.gray, 'gray'), 'color_gray_')
+      : createColorRamp(palette.gray, 'color_gray_');
+  const themeRamp =
+    colors && colors.theme
+      ? createColorRamp(getRamp(colors.theme, 'theme'), 'color_theme_')
+      : createColorRamp(palette.brand, 'color_theme_');
+
   return {
-    ...themeFromTokens({ themeRamp, tokens }),
-    ...nonTokenVariables,
+    ...themeFromTokens(tokens),
+    ...nonTokenVariables(colors),
 
-    ...getThemeRamp(themeRamp),
-    color_white: white,
+    ...(colors ? colorOverrides(colors) : undefined),
+    ...(colors ? primaryColorsByVariation(colors) : undefined),
+
     ...grayRamp,
-    color_black: black,
+    ...themeRamp,
+    color_black: (colors && colors.black) || palette.black,
+    color_white: (colors && colors.white) || palette.white,
 
-    ...overrides
+    ...(options && options.overrides)
   };
 }
--- src/library/themes/themeFromTokens.js
+++ src/library/themes/themeFromTokens.js
@@ -1,52 +1,67 @@
 /* @flow */
-import colorAliases from 'mineral-ui-tokens/colorAliases';
 import pxToEm from '../styles/pxToEm';
+import colorAliases from './generated/colorAliases';
 
-type Args = { themeRamp?: Ramp, tokens: Tokens };
-export type Ramp = { [string]: string };
-export type Theme = Tokens;
+type Aliases = { [string]: string };
+type ColorOptions = {
+  aliases?: Aliases,
+  color: Ramp | string,
+  colorName: string,
+  newKey?: (string) => string,
+  tokens: ColorTokens
+};
+type ColorTheme = ColorTokens;
+type ColorTokens = { [string]: string };
+export type Ramp = { [number]: string };
+type Theme = Tokens;
 type Tokens = { [string]: number | string };
 
 const contains = (string: string, subString: string) =>
   string.indexOf(subString) !== -1;
 
+const newKey = (key) => key.replace('brand', 'theme');
+
 const remToEm = (value: string) => value.replace('rem', 'em');
 
-/*
- * Tokens can have "brand" values, e.g. `color_brand: 'brand_60'`. This creates
- * a map between those keys and the appropriate `color_theme` (60, above), which
- * is then used when a non-default base color is used to create a theme. In the
- * default case, the `color_brand` token's value is simply used directly.
- */
-const themeRampMap = Object.keys(colorAliases)
-  .filter((key) => key.indexOf('brand') !== -1)
-  .reduce((acc, key) => {
-    acc[key] = colorAliases[key].replace('brand', 'color_theme');
-    return acc;
-  }, {});
-
-export default function themeFromTokens({ themeRamp, tokens }: Args): Theme {
+export default function(tokens: Tokens): Theme {
   return Object.keys(tokens).reduce((acc, key) => {
-    const isBrandKey = contains(key, 'brand');
-    const value = tokens[key];
-
-    const newKey = isBrandKey ? key.replace('brand', 'theme') : key;
-    let newValue = value;
-
+    let value = tokens[key];
     if (typeof value === 'string') {
-      if (isBrandKey) {
-        newValue = (themeRamp && themeRamp[themeRampMap[key]]) || tokens[key];
+      if (value.split('px').length === 2 && !contains(key, 'breakpoint')) {
+        value = pxToEm(value);
       } else if (contains(key, 'fontSize')) {
-        newValue = remToEm(value);
-      } else if (
-        value.split('px').length === 2 &&
-        !contains(key, 'breakpoint')
-      ) {
-        newValue = pxToEm(value);
+        value = remToEm(value);
       }
     }
 
-    acc[newKey] = newValue;
+    acc[newKey(key)] = value;
+    return acc;
+  }, {});
+}
+
+export function themeFromColorTokens({
+  aliases = colorAliases,
+  color,
+  colorName,
+  tokens
+}: ColorOptions): ColorTheme {
+  return Object.keys(tokens).reduce((acc, key) => {
+    let value = tokens[key];
+    if (typeof color === 'string') {
+      value = color;
+    } else {
+      const rampKey = parseInt(aliases[key].split('_')[1]);
+      if (rampKey) {
+        if (color[rampKey]) {
+          value = color[rampKey];
+        } else {
+          throw new Error(`[mineral-ui/themes/createTheme]: colors.${colorName}[${rampKey}] is missing.
+  See https://github.com/mineral-ui/mineral-ui/blob/master/packages/mineral-ui-tokens/src/blue.js for an example color ramp.`);
+        }
+      }
+    }
+
+    acc[newKey(key)] = value;
     return acc;
   }, {});
 }
