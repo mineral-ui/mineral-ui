3d8335cddcb951a5c3f51230d263665fd7538a70
Brent Ertz <brent.ertz@ca.com>
2018-05-17 08:07:47 -0600

    WIP: Add more content to scrolling content example

--- src/website/app/demos/Dialog/examples/scrolling.js
+++ src/website/app/demos/Dialog/examples/scrolling.js
@@ -1,4 +1,5 @@
 /* @flow */
+import React from 'react';
 import Button from '../../../../../library/Button';
 import {
   DialogBody,
@@ -8,6 +9,21 @@ import {
 } from '../../../../../library/Dialog';
 import DemoLayout from '../components/DemoLayout';
 import Dialog from '../components/DemoDialog';
+
+const content = [...Array(6)].map((_, index) => (
+  <p key={index}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo
+    laoreet sit amet. Vitae sapien pellentesque habitant morbi tristique.
+    Posuere lorem ipsum dolor sit. Vel risus commodo viverra maecenas accumsan.
+    Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Lectus
+    mauris ultrices eros in cursus turpis. Natoque penatibus et magnis dis. Eget
+    aliquet nibh praesent tristique magna sit amet. Pellentesque elit eget
+    gravida cum sociis natoque penatibus. Luctus accumsan tortor posuere ac ut
+    consequat semper viverra. Sed vulputate odio ut enim. Vivamus at augue eget
+    arcu.
+  </p>
+));
 
 export default {
   id: 'scrolling',
@@ -20,7 +36,8 @@ export default {
     DialogBody,
     DialogFooter,
     DialogHeader,
-    DialogTitle
+    DialogTitle,
+    content
   },
   source: `
     <DemoLayout>
@@ -31,45 +48,7 @@ export default {
           </DialogTitle>
         </DialogHeader>
         <DialogBody>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Viverra nam libero justo laoreet sit amet. Vitae sapien
-            pellentesque habitant morbi tristique. Posuere lorem ipsum dolor
-            sit. Vel risus commodo viverra maecenas accumsan. Id cursus
-            metus aliquam eleifend mi in nulla posuere sollicitudin. Lectus
-            mauris ultrices eros in cursus turpis. Natoque penatibus et
-            magnis dis. Eget aliquet nibh praesent tristique magna sit amet.
-            Pellentesque elit eget gravida cum sociis natoque penatibus.
-            Luctus accumsan tortor posuere ac ut consequat semper viverra.
-            Sed vulputate odio ut enim. Vivamus at augue eget arcu.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Viverra nam libero justo laoreet sit amet. Vitae sapien
-            pellentesque habitant morbi tristique. Posuere lorem ipsum dolor
-            sit. Vel risus commodo viverra maecenas accumsan. Id cursus
-            metus aliquam eleifend mi in nulla posuere sollicitudin. Lectus
-            mauris ultrices eros in cursus turpis. Natoque penatibus et
-            magnis dis. Eget aliquet nibh praesent tristique magna sit amet.
-            Pellentesque elit eget gravida cum sociis natoque penatibus.
-            Luctus accumsan tortor posuere ac ut consequat semper viverra.
-            Sed vulputate odio ut enim. Vivamus at augue eget arcu.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Viverra nam libero justo laoreet sit amet. Vitae sapien
-            pellentesque habitant morbi tristique. Posuere lorem ipsum dolor
-            sit. Vel risus commodo viverra maecenas accumsan. Id cursus
-            metus aliquam eleifend mi in nulla posuere sollicitudin. Lectus
-            mauris ultrices eros in cursus turpis. Natoque penatibus et
-            magnis dis. Eget aliquet nibh praesent tristique magna sit amet.
-            Pellentesque elit eget gravida cum sociis natoque penatibus.
-            Luctus accumsan tortor posuere ac ut consequat semper viverra.
-            Sed vulputate odio ut enim. Vivamus at augue eget arcu.
-          </p>
+          {content}
         </DialogBody>
         <DialogFooter>
           <Button minimal size="medium">Cancel</Button>
