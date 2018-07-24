749c62cb75d9e27f16ad286c5e7c22f801ebf0a3
Brent Ertz <brent.ertz@ca.com>
2018-07-24 14:11:56 -0600

    test(checkbox): Add indeterminate specs
    Co-authored-by: Kyle Gach <kyle.gach@ca.com>

--- src/library/Checkbox/__tests__/Checkbox.spec.js
+++ src/library/Checkbox/__tests__/Checkbox.spec.js
@@ -1,12 +1,49 @@
 /* @flow */
-import React from 'react';
-import { shallow } from 'enzyme';
+import React, { Component } from 'react';
+import { mount, shallow } from 'enzyme';
+import { ThemeProvider } from '../../themes';
 import Checkbox from '../Checkbox';
 import examples from '../../../website/app/demos/Checkbox/examples';
 import testDemoExamples from '../../../../utils/testDemoExamples';
 
-function shallowCheckbox() {
-  return shallow(<Checkbox label="example" />);
+const defaultProps = {
+  label: 'example'
+};
+
+function shallowCheckbox(props = {}) {
+  const checkboxProps = {
+    ...defaultProps,
+    ...props
+  };
+  return shallow(<Checkbox {...checkboxProps} />);
+}
+
+const mountApp = (props = {}) => {
+  const appProps = {
+    ...defaultProps,
+    ...props
+  };
+
+  return mount(<App {...appProps} />);
+};
+
+class App extends Component<*, *> {
+  state = {
+    indeterminate: this.props.indeterminate
+  };
+
+  render() {
+    const checkboxProps = {
+      ...this.props,
+      indeterminate: this.state.indeterminate
+    };
+
+    return (
+      <ThemeProvider>
+        <Checkbox {...checkboxProps} />
+      </ThemeProvider>
+    );
+  }
 }
 
 describe('Checkbox', () => {
@@ -16,5 +53,86 @@ describe('Checkbox', () => {
     const checkbox = shallowCheckbox();
 
     expect(checkbox.exists()).toEqual(true);
+  });
+
+  describe('indeterminate', () => {
+    let app, checkbox;
+    const onClick = jest.fn();
+    const onChange = jest.fn();
+
+    beforeEach(() => {
+      onClick.mockReset();
+      onChange.mockReset();
+    });
+
+    describe('when uncontrolled', () => {
+      beforeEach(() => {
+        app = mountApp({ defaultIndeterminate: true, onClick, onChange });
+        checkbox = app.find('input');
+      });
+
+      it('is initialized correctly', () => {
+        expect(checkbox.instance().indeterminate).toEqual(true);
+        expect(checkbox.instance().checked).toEqual(true);
+      });
+
+      describe('when clicked', () => {
+        it('toggles indeterminate and checked state', () => {
+          checkbox.simulate('click');
+          app.update();
+          checkbox = app.find('input').instance();
+
+          expect(checkbox.indeterminate).toEqual(false);
+          expect(checkbox.checked).toEqual(false);
+        });
+
+        it('calls onChange handler when provided', () => {
+          checkbox.simulate('click');
+
+          expect(onChange).toHaveBeenCalledTimes(1);
+        });
+
+        it('calls onClick handler when provided', () => {
+          checkbox.simulate('click');
+
+          expect(onClick).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
+
+    describe('controlled', () => {
+      beforeEach(() => {
+        app = mountApp({ indeterminate: true, onClick, onChange });
+        checkbox = app.find('input');
+      });
+
+      it('is initialized correctly', () => {
+        expect(checkbox.instance().indeterminate).toEqual(true);
+        expect(checkbox.instance().checked).toEqual(true);
+      });
+
+      describe('when clicked', () => {
+        it('does not toggle indeterminate or checked state', () => {
+          checkbox.simulate('click');
+          app.update();
+          checkbox = app.find('input').instance();
+
+          expect(checkbox.indeterminate).toEqual(true);
+          expect(checkbox.checked).toEqual(true);
+        });
+
+        it('calls onChange handler when provided', () => {
+          checkbox.simulate('click');
+
+          expect(onChange).toHaveBeenCalledTimes(1);
+        });
+
+        it('calls onClick handler when provided', () => {
+          checkbox.simulate('click');
+
+          expect(onChange).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
   });
 });
