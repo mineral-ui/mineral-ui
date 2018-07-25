f957480e3ddfb33b4ef3d0dfb80e8aa9169cc3e9
Brent Ertz <brent.ertz@ca.com>
2018-07-25 12:24:01 -0600

    fix(table): Fix theme overrides
    * Resolves incorrect override behavior between TableCell and TableHeaderCell theme variables
    
    Co-authored-by: Kyle Gach <kyle.gach@ca.com>

--- src/library/themes/mapComponentThemes.js
+++ src/library/themes/mapComponentThemes.js
@@ -1,11 +1,28 @@
 /* @flow */
 /**
  * Generates a new component theme based on the theme of another component.
+ *
+ * preserveKeys
+ *
+ *   What does it do:
+ *
+ *     Ensures source theme variables are used in override styles when the
+ *     source does not apply those styles and the override component is
+ *     independently themeable
+ *
+ *     e.g. Necessary in TableHeaderCell (which is a themed TableCell) because
+ *     TableCells and TableHeaderCells are both independently themeable
+ *
+ *   How to use:
+ *
+ *     Provide array of theme variable keys used in style properties applied by
+ *     source component and not by override component
  */
 export default function mapComponentThemes(
   source: { name: string, theme: Object },
   override: { name: string, theme: Object },
-  baseTheme: Object
+  baseTheme: Object,
+  preserveKeys?: Array<string>
 ) {
   const REGEXP_SOURCE_NAME = new RegExp(`^${source.name}`);
   const sourceThemeWithRenamedKeys = Object.keys(source.theme).reduce(
@@ -21,10 +38,18 @@ export default function mapComponentThemes(
     },
     {}
   );
+  const preserved =
+    preserveKeys &&
+    preserveKeys.reduce((acc, sourceKey) => {
+      const keyProperty = sourceKey.split('_')[1];
+      acc[`${override.name}_${keyProperty}`] = source.theme[sourceKey];
+      return acc;
+    }, {});
 
   return {
     ...sourceThemeWithRenamedKeys,
     ...override.theme,
-    ...baseTheme
+    ...baseTheme,
+    ...preserved
   };
 }
