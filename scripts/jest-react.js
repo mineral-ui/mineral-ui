407f6cc8aed3b27a7b4593552e41bcb05451e351
Brent Ertz <brent.ertz@ca.com>
2018-08-13 10:29:43 -0600

    chore(dependencies): Update dev react version

--- scripts/jest-react.js
+++ scripts/jest-react.js
@@ -10,7 +10,7 @@ const semver = require('semver');
 const argv = require('yargs')
   .option('version', {
     describe: 'Version of React to use',
-    default: ['15.3.2', '15.4.2', '15.5.4', '15.6.2', '16.3.0'],
+    default: ['15.3.2', '15.4.2', '15.5.4', '15.6.2', '16.4.2'],
     type: 'string'
   })
   .array('version')
