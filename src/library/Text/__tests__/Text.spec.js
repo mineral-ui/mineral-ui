b2af84fe22d609e87b22338453ff86a3e409c13b
Victoria Vasys <victoria.vasys@gmail.com>
2018-08-27 16:12:23 -0600

    test(text): Add coverage for textWithThemeOverrides

--- src/library/Text/__tests__/Text.spec.js
+++ src/library/Text/__tests__/Text.spec.js
@@ -2,13 +2,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Text from '../Text';
+import textWithThemeOverrides from '../textWithThemeOverrides';
 import examples from '../../../website/app/demos/Text/examples';
+import renderComponentStylesToString from '../../../../utils/renderComponentStylesToString';
 import testDemoExamples from '../../../../utils/testDemoExamples';
 import testThemeOverrides from '../../../../utils/testThemeOverrides';
 
 function shallowText(props = {}) {
   return shallow(<Text {...props}>A</Text>);
 }
+
+const theme = {
+  TestComponent_color: 'red',
+  TestComponent_fontSize: '2em',
+  TestComponent_fontWeight: 800
+};
 
 describe('Text', () => {
   testDemoExamples(examples);
@@ -26,5 +34,21 @@ describe('Text', () => {
       'Text_lineHeight',
       'Text_marginBottom'
     ]);
+  });
+
+  describe('textWithThemeOverrides function', () => {
+    const themedText = textWithThemeOverrides({
+      appearance: 'h1',
+      children: 'Test',
+      displayName: 'TestComponent',
+      textComponent: Text,
+      theme
+    });
+
+    const themedTextStyles = renderComponentStylesToString(themedText);
+
+    expect(themedTextStyles.includes('color:red')).toEqual(true);
+    expect(themedTextStyles.includes('font-size:32px')).toEqual(true);
+    expect(themedTextStyles.includes('font-weight:800')).toEqual(true);
   });
 });
