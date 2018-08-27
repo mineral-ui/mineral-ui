2b2b3f7841c2926065d613f2ad546484787ff54a
Brent Ertz <brent.ertz@gmail.com>
2018-08-27 16:05:53 -0600

    chore(icon): Add internal close icon

---
+++ src/library/Icon/IconClose.js
@@ -0,0 +1,29 @@
+/* @flow */
+import React from 'react';
+import Icon from 'mineral-ui/Icon';
+
+type Props = {
+  size?: string | 'small' | 'medium' | 'large',
+  color?: string,
+  rtl?: boolean,
+  title?: string
+};
+
+/* eslint-disable prettier/prettier */
+export default function IconClose(props: Props) {
+  const iconProps = {
+    rtl: false,
+    ...props
+  };
+
+  return (
+    <Icon {...iconProps}>
+      <g>
+        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+      </g>
+    </Icon>
+  );
+}
+
+IconClose.displayName = 'IconClose';
+IconClose.category = 'navigation';
