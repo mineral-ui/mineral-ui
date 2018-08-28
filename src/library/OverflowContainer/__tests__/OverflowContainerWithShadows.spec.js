e775735c240d5edeae84f7ddd17213cb84bab4b0
Victoria Vasys <victoria.vasys@gmail.com>
2018-08-28 15:53:10 -0600

    fix(dialog): DialogBody content overflows correctly
    * Refactor OverflowContainerWithShadows to place shadows _over_ the overflowed content and apply in x & y axes
    * Remove style reset from Popover root

--- src/library/OverflowContainer/__tests__/OverflowContainerWithShadows.spec.js
+++ src/library/OverflowContainer/__tests__/OverflowContainerWithShadows.spec.js
@@ -8,8 +8,26 @@ import OverflowContainerWithShadows, {
 import { mountInThemeProvider } from '../../../../utils/enzymeUtils';
 import testThemeOverrides from '../../../../utils/testThemeOverrides';
 
-const mountOverflowContainerWithShadows = (props = {}) =>
-  mountInThemeProvider(<OverflowContainerWithShadows {...props} />);
+/*
+ * [1] This getter does not exist on the HTMLElement.prototype in JSDOM, so we
+ *     must mock it on the global. `clientWidth` and `offsetLeft`, also used by
+ *     OverflowContainer always return 0, which is conveniently useful.
+ * [2] Then we must clean up our global mocking in [1]
+ */
+
+const defaultProps = {
+  scrollX: true
+};
+
+const mountOverflowContainerWithShadows = (props = {}) => {
+  const containerProps = {
+    ...defaultProps,
+    ...props
+  };
+  return mountInThemeProvider(
+    <OverflowContainerWithShadows {...containerProps} />
+  );
+};
 
 describe('OverflowContainerWithShadows', () => {
   it('renders', () => {
@@ -18,20 +36,52 @@ describe('OverflowContainerWithShadows', () => {
     expect(overflowContainer.exists()).toEqual(true);
   });
 
-  // TODO: I suspect that we cannot use findDOMNode inside testThemeOverrides,
-  //       which would prevent either shadow variable from ever applying. If so,
-  //       we don't need the fixed heights below.
   describe('theme overrides', () => {
     testThemeOverrides(
-      <OverflowContainerWithShadows style={{ height: 100 }}>
-        <div style={{ height: 1000 }} />
-      </OverflowContainerWithShadows>,
+      <OverflowContainerWithShadows {...defaultProps} />,
       getProcessedComponentThemeKeys(componentTheme(mineralTheme), {
         excludeKeys: [
           'OverflowContainerWithShadows_boxShadowBottom',
+          'OverflowContainerWithShadows_boxShadowLeft',
+          'OverflowContainerWithShadows_boxShadowRight',
           'OverflowContainerWithShadows_boxShadowTop'
         ]
       })
     );
+  });
+
+  it('does not apply shadow when not scrollable', () => {
+    const [, overflowContainer] = mountInThemeProvider(
+      <OverflowContainerWithShadows {...defaultProps} />
+    );
+
+    expect(overflowContainer).toMatchSnapshot();
+  });
+
+  it('applies shadow when scrollable', () => {
+    // [1]
+    // $FlowFixMe
+    Object.defineProperties(window.HTMLElement.prototype, {
+      scrollWidth: {
+        // Note that this mocked getter is designed to always trigger overflow
+        // because it's greater than offsetLeft + offsetWidth
+        get: () => 10,
+        configurable: true
+      }
+    });
+
+    const [, overflowContainer] = mountInThemeProvider(
+      <OverflowContainerWithShadows {...defaultProps} />
+    );
+
+    // [2]
+    // $FlowFixMe
+    Object.defineProperties(window.HTMLElement.prototype, {
+      scrollWidth: {
+        get: undefined
+      }
+    });
+
+    expect(overflowContainer).toMatchSnapshot();
   });
 });
