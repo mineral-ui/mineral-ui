1b543389e35757575ac572453ea39dc9bfcb0fa3
Victoria Vasys <victoria.vasys@gmail.com>
2018-09-26 14:08:03 -0600

    refactor(button,buttongroup,menuitem): Update per new styles
    * Button: default styles and implement new RTL property pattern
    * ButtonGroup affected mainly due to new focus styles with border colors
        * Implement new RTL property pattern
        * Add comments for complex selectors
    * MenuItem affected due to tokens change

--- src/library/Menu/MenuItem.js
+++ src/library/Menu/MenuItem.js
@@ -46,9 +46,9 @@ type RenderProps = {
 // come from Button (large)
 // prettier-ignore
 export const componentTheme = (baseTheme: Object) => ({
-  MenuItem_backgroundColor_active: baseTheme.backgroundColor_active,
-  MenuItem_backgroundColor_focus: baseTheme.backgroundColor_focus,
-  MenuItem_backgroundColor_hover: baseTheme.backgroundColor_hover,
+  MenuItem_backgroundColor_active: baseTheme.gray_20,
+  MenuItem_backgroundColor_focus: baseTheme.gray_10,
+  MenuItem_backgroundColor_hover: baseTheme.gray_10,
   MenuItem_backgroundColor_selected: baseTheme.backgroundColor_theme_selected,
   MenuItem_backgroundColor_selectedActive: baseTheme.backgroundColor_theme_selectedActive,
   MenuItem_backgroundColor_selectedHover: baseTheme.backgroundColor_theme_selectedHover,
