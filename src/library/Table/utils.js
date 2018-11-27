1891df902fe79c82914bf96ff6d241088d0df326
Brent Ertz <brent.ertz@gmail.com>
2018-11-27 08:05:55 -0700

    refactor(Table): Update legacy lifecycle methods
    * Some utils were extracted into a separate Table/utils.js in order to be able to spy/assert on them in Jest.
    * Adds `react-lifecycles-compat` polyfill, which is needed until we drop support for older React versions. This can be removed once our minimum React version is at least 16.3.

---
+++ src/library/Table/utils.js
@@ -0,0 +1,46 @@
+/* @flow */
+import type { Rows, TableProps } from './types';
+
+export const getColumns = ({ columns, data }: TableProps) => {
+  return (
+    columns ||
+    (data[0]
+      ? Object.keys(data[0]).reduce((acc, cell) => {
+          acc.push({ content: cell, key: cell });
+          return acc;
+        }, [])
+      : [])
+  );
+};
+
+export const getComparators = ({ columns }: TableProps) => {
+  const comparators =
+    columns &&
+    columns.reduce((acc, column) => {
+      const { key, sortComparator } = column;
+      if (sortComparator) {
+        acc[key] = sortComparator;
+      }
+      return acc;
+    }, {});
+
+  return comparators && Object.keys(comparators).length
+    ? comparators
+    : undefined;
+};
+
+export const getSelectableRows = (rows: Rows) =>
+  rows.filter((row) => !row.disabled);
+
+export const getSortable = ({
+  columns,
+  defaultSort,
+  sort,
+  sortable
+}: TableProps) =>
+  Boolean(
+    defaultSort ||
+      sort ||
+      sortable ||
+      (columns && columns.some((column) => column.sortable))
+  );
