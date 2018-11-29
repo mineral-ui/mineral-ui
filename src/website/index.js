83a77d95a5db4be2bffe0367b5ee59b0b2203062
Brent Ertz <brent.ertz@broadcom.com>
2018-11-29 11:43:14 -0700

    chore(dependencies): Update babel

--- src/website/index.js
+++ src/website/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'babel-polyfill';
+import '@babel/polyfill';
 import App from './app/App';
 require('./index.css');
 
