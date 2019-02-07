fefe914a442360b2676afd38720fb956365ba09c
Brent Ertz <brent.ertz@broadcom.com>
2019-02-07 11:37:04 -0700

    WIP: Refactor utils/children types
    * Contains a FIXME

--- src/library/utils/children.ts
+++ src/library/utils/children.ts
@@ -2,6 +2,9 @@
 import { Children } from 'react';
 import { toArray } from './collections';
 
+const isElement = (subject: any): subject is React.ReactElement<any> =>
+  typeof subject === 'object' && subject.hasOwnProperty('type');
+
 const hasChildren = (child: React.ReactElement<any>): boolean =>
   Boolean(child && child.props && child.props.children);
 
@@ -12,20 +15,21 @@ export const findDeep = (
   children: React.ReactNode,
   finder: (element: React.ReactElement<any>) => boolean
 ): React.ReactElement<any> | null | undefined =>
-  Children.toArray(children).find((child) =>
-    hasComplexChildren(child)
-      ? findDeep(child.props.children, finder)
-      : finder(child)
+  Children.toArray(children).filter(isElement).find((child) =>
+      // @ts-ignore FIXME - issue in recursive call on line 20
+      hasComplexChildren(child)
+        ? findDeep(child.props.children, finder)
+        : finder(child)
   );
 
 export function findByType(
-  children: React.ReactNode | null | undefined,
+  children: React.ReactNode,
   type: React.ComponentType
 ): React.ReactElement<any> | null | undefined {
-  let match;
+  let match: React.ReactElement<any> | null | undefined;
 
   Children.forEach(children, (child) => {
-    if (!match && child && child.type === type) {
+    if (!match && child && isElement(child) && child.type === type) {
       match = child;
     }
   });
@@ -34,23 +38,23 @@ export function findByType(
 }
 
 export function findAllByType(
-  children: React.ReactNode | null | undefined,
+  children: React.ReactNode,
   type: React.ComponentType
 ): Array<React.ReactElement<any>> | null | undefined {
   return Children.map(children, (child) => {
-    if (child && child.type === type) {
+    if (isElement(child) && child.type === type) {
       return child;
     }
   });
 }
 
 export function excludeByType(
-  children: React.ReactNode | null | undefined,
+  children: React.ReactNode,
   type: React.ComponentType | Array<React.ComponentType>
 ): Array<React.ReactElement<any>> | null | undefined {
   const types = toArray(type);
   return Children.map(children, (child) => {
-    if (types.indexOf(child.type) === -1) {
+    if (isElement(child) && types.indexOf(child.type) === -1) {
       return child;
     }
   });
