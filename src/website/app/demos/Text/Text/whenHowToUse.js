5abbc4b7fd4937a85322e11f4bd0def194dfe7cc
Avétis KAZARIAN <avetis.k@gmail.com>
2019-03-15 17:03:11 +0100

    Update whenHowToUse.js
    Use new property `as` instead of `element`.

--- src/website/app/demos/Text/Text/whenHowToUse.js
+++ src/website/app/demos/Text/Text/whenHowToUse.js
@@ -3,4 +3,4 @@ export default `Use Text when displaying text-based content, particularly
 headings or content that wraps to multiple lines, in your app to ensure proper
 font sizes, colors, weights, and spacing relative to other components.
 
-The semantic meaning of your content should inform the \`element\` prop.`;
+The semantic meaning of your content should inform the \`as\` prop.`;
